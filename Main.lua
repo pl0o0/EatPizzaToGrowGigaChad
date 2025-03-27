e4c1d5e0c5d9cd2b1d6e2faf9b67114018ab9541
pl0o0 <robloxfruits0111@gmail.com>
2025-03-27 23:28:09 +0900

    Main.lua

--- Main.lua
+++ Main.lua
@@ -30,16 +30,8 @@ local args8 = {
     [1] = "\230\142\137\232\144\189\231\137\169",
     [2] = "2201"
 }
-local args9 = {
-    [1] = "\230\142\137\232\144\189\231\137\169",
-    [2] = "2201"
-}
-local args10 = {
-    [1] = "\230\142\137\232\144\189\231\137\169",
-    [2] = "2201"
-}
 
-while wait(0.0001) do
+while wait(0.001) do
 game:GetService("ReplicatedStorage").Msg:FindFirstChild("\229\144\131"):FireServer(unpack(args))
 game:GetService("ReplicatedStorage").Msg:FindFirstChild("\229\144\131"):FireServer(unpack(args2))
 game:GetService("ReplicatedStorage").Msg:FindFirstChild("\229\144\131"):FireServer(unpack(args3))
@@ -48,6 +40,4 @@ game:GetService("ReplicatedStorage").Msg:FindFirstChild("\229\144\131"):FireServ
 game:GetService("ReplicatedStorage").Msg:FindFirstChild("\229\144\131"):FireServer(unpack(args6))
 game:GetService("ReplicatedStorage").Msg:FindFirstChild("\229\144\131"):FireServer(unpack(args7))
 game:GetService("ReplicatedStorage").Msg:FindFirstChild("\229\144\131"):FireServer(unpack(args8))
-game:GetService("ReplicatedStorage").Msg:FindFirstChild("\229\144\131"):FireServer(unpack(args9))
-game:GetService("ReplicatedStorage").Msg:FindFirstChild("\229\144\131"):FireServer(unpack(args10))
 end
