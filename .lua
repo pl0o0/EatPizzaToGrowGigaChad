30c09672300ed8dac0f0fd49dd55773b2bb66300
pl0o0 <robloxfruits0111@gmail.com>
2025-03-27 21:21:37 +0900

    CreatedByWpl0o0.lua

---
+++ .lua
@@ -0,0 +1,52 @@
+local args = {
+    [1] = "\230\142\137\232\144\189\231\137\169",
+    [2] = "2201"
+}
+local args2 = {
+    [1] = "\230\142\137\232\144\189\231\137\169",
+    [2] = "2201"
+}
+local args3 = {
+    [1] = "\230\142\137\232\144\189\231\137\169",
+    [2] = "2201"
+}
+local args4 = {
+    [1] = "\230\142\137\232\144\189\231\137\169",
+    [2] = "2201"
+}
+local args5 = {
+    [1] = "\230\142\137\232\144\189\231\137\169",
+    [2] = "2201"
+}
+local args6 = {
+    [1] = "\230\142\137\232\144\189\231\137\169",
+    [2] = "2201"
+}
+local args7 = {
+    [1] = "\230\142\137\232\144\189\231\137\169",
+    [2] = "2201"
+}
+local args8 = {
+    [1] = "\230\142\137\232\144\189\231\137\169",
+    [2] = "2201"
+}
+local args9 = {
+    [1] = "\230\142\137\232\144\189\231\137\169",
+    [2] = "2201"
+}
+local args10 = {
+    [1] = "\230\142\137\232\144\189\231\137\169",
+    [2] = "2201"
+}
+while wait(0.000001) do
+game:GetService("ReplicatedStorage").Msg:FindFirstChild("\229\144\131"):FireServer(unpack(args))
+game:GetService("ReplicatedStorage").Msg:FindFirstChild("\229\144\131"):FireServer(unpack(args2))
+game:GetService("ReplicatedStorage").Msg:FindFirstChild("\229\144\131"):FireServer(unpack(args3))
+game:GetService("ReplicatedStorage").Msg:FindFirstChild("\229\144\131"):FireServer(unpack(args4))
+game:GetService("ReplicatedStorage").Msg:FindFirstChild("\229\144\131"):FireServer(unpack(args5))
+game:GetService("ReplicatedStorage").Msg:FindFirstChild("\229\144\131"):FireServer(unpack(args6))
+game:GetService("ReplicatedStorage").Msg:FindFirstChild("\229\144\131"):FireServer(unpack(args7))
+game:GetService("ReplicatedStorage").Msg:FindFirstChild("\229\144\131"):FireServer(unpack(args8))
+game:GetService("ReplicatedStorage").Msg:FindFirstChild("\229\144\131"):FireServer(unpack(args9))
+game:GetService("ReplicatedStorage").Msg:FindFirstChild("\229\144\131"):FireServer(unpack(args10))
+end
